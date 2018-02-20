5336e1513210e980eb592bc9de4e249aba8ba910
kellmacartney <33106247+kellmacartney@users.noreply.github.com>
2018-02-20 10:49:28 +1000

    buggy movement
    buggy movement constraints added

---
+++ Where for Art Thou/Assets/Scripts/MovementConstraints.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementConstraints : MonoBehaviour
+{
+    public float buggySpeed;
+    public Vector2 xCon;
+    public Vector2 yCon;
+    Vector2 wantPos;
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        wantPos += new Vector2(Input.GetAxis("Horizontal") * buggySpeed * Time.deltaTime, Input.GetAxis("Vertical") * buggySpeed * Time.deltaTime);
+        transform.position = new Vector3(Mathf.Clamp(wantPos.x, xCon.x, xCon.y), Mathf.Clamp(wantPos.y, yCon.x, yCon.y), transform.position.z);
+	}
+}
